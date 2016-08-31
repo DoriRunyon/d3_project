c161e219998a7c0e96d54e76e56409a70aa8501c
DoriRunyon <dorileerunyon@gmail.com>
2016-08-30 20:00:15 -0700

    Cleaned up server.py and created models

--- model.py
+++ model.py
@@ -1,12 +1,6 @@
-"""Models and database functions for Broadcast."""
+"""Models and database functions for spotify discovery project."""
 
 from flask_sqlalchemy import SQLAlchemy
-
-
-
-# Here's where we create the idea of our database. We're getting this through
-# the Flask-SQLAlchemy library. On db, we can find the `session`
-# object, where we do most of our interactions (like committing, etc.)
 
 db = SQLAlchemy()
 
@@ -15,34 +9,62 @@ db = SQLAlchemy()
 # Part 1: Compose ORM
 
 
-def example_data():
-    """Create some sample data."""
+class User(db.Model):
+    "User info."
 
-    # In case this is run more than once, empty out existing data
-    User.query.delete()
+    __tablename__ = "users"
 
-    # Add sample employees and departments
-    admin = User(email='admin', password='default')
+    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(50), nullable=False)
 
-    db.session.add_all([admin])
-    db.session.commit()
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return "<Event user_id=%s email =%s>" % (self.user_id,
+                                                 self.email)
+
+
+class Track(db.Model):
+    "Track info."
+
+    __tablename__ = "tracks"
+
+    track_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    spotify_track_id = db.Column(db.Integer, nullable=False)
+    datetime = db.Column(db.DateTime, nullable=True) #is this the best to use if I just want to save the date? Not the time
+
+    def __repr__(self):
+        """Provide helpful representation when printed."""
+
+        return "<Track track_id=%s date =%s>" % (self.track_id,
+                                                 self.date)
+
+class User_Track(db.Model):
+    """Association table for tracks user has saved."""
+
+    __tablename__ = "user_tracks"
+
+    user_track_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    track_id = db.Column(db.Integer, db.ForeignKey('track.track_id'), nullable=False)
 
 
 def connect_to_db(app, db_uri="postgresql:///noodledb"):
     """Connect the database to our Flask app."""
 
-    # Configure to use our postgres database
+    # Configure to use postgres database
     app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+
     # app.config['SQLALCHEMY_ECHO'] = True
     db.app = app
     db.init_app(app)
 
 
 if __name__ == "__main__":
-    # As a convenience, if we run this module interactively, it will leave
+    # If this module is run interactively, it will leave
     # you in a state of being able to work with the database directly.
 
-    # So that we can use Flask-SQLAlchemy, we'll make a Flask app
+    # To use Flask-SQLAlchemy, make a Flask app
     from flask import Flask
     app = Flask(__name__)
 
--- server.py
+++ server.py
@@ -1,16 +1,32 @@
+"""Discover Playlist Project Server"""
 
+# Standard Python Libraries
+import os
+
+# Flask
 from flask import Flask, render_template, redirect, request, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
-from model import connect_to_db, db
 from jinja2 import StrictUndefined
 
+# Models
+from model import connect_to_db, db
 
+# Other External Libraries
+import spotipy
+spotify = spotipy.Spotify()
+
+# Setting up Flask app
 app = Flask(__name__)
-
-app.secret_key = "ABC"
 app.jinja_env.undefined = StrictUndefined
+app.secret_key = os.environ['APP_SECRET']
 
 
+# API key and secret
+spotify_consumer_key = os.environ['SPOTIFY_CONSUMER_KEY']
+spotify_consumer_secret = os.environ['SPOTIFY_CONSUMER_SECRET']
+
+
+# App Routes
 
 @app.route('/')
 def index():
@@ -19,17 +35,14 @@ def index():
     return render_template("index.html")
 
 
+# Running App
+
 if __name__ == "__main__":
-    # We have to set debug=True here, since it has to be True at the point
-    # that we invoke the DebugToolbarExtension
-    # app.debug = True
+
+    app.debug = True
 
     connect_to_db(app)
 
-    # Use the DebugToolbar
     DebugToolbarExtension(app)
 
     app.run()
-
-    import doctest
-    doctest.testmod()
